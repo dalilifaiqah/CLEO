55961b1dd99bc5c39adbd7cea62756498594bbdc
zeynanz <88524435+zeynanz@users.noreply.github.com>
2021-09-06 22:09:30 +0800

    database update
    still not working

--- Moderna-pro/colorlib-regform-9/colorlib-regform-9/CustomerLogin.php
+++ Moderna-pro/colorlib-regform-9/colorlib-regform-9/CustomerLogin.php
@@ -1,60 +1,3 @@
-<?php 
-
-session_start();
-
-	include("connection.php");
-	include("functions.php");
-
-
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-
-		if(!empty($email) && !empty($password))
-		{
-
-			//read from database
-			$query = "select * from customer where email = '$email' limit 1";
-			$result = mysqli_query($con, $query);
-
-			if($result)
-			{
-				if($result && mysqli_num_rows($result) > 0)
-				{
-
-					$user_data = mysqli_fetch_assoc($result);
-					
-					if($user_data['password'] === $password)
-					{
-
-						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: Guesthomepage.html");
-						die;
-					}
-				}
-			}
-			
-			echo "wrong username or password!";
-		}else
-		{
-			echo "wrong username or password!";
-		}
-	}
-
-?>
-
-
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -76,14 +19,14 @@ session_start();
         
         <div class="container">
             <div class="signup-content">
-                <form method="POST" id="signup-form" class="signup-form">
+                <form action=/MasterCLEO/Moderna-pro/colorlib-regform-9/colorlib-regform-9/CustomerDB.php method="POST" id="signup-form" class="signup-form">
                     <h2>Log In </h2>
                     <p class="desc">Log into an existing account in <span>CLEO</span></p>
                     <div class="form-group">
                         <!--<input type="text" class="form-input" name="name" id="name" placeholder="Your Name"/>-->
                     </div>
                     <div class="form-group">
-                        <input type="email" class="form-input" name="email" id="email" placeholder="Email / Username"/>
+                        <input type="email" class="form-input" name="email" id="email" placeholder="Email"/>
                     </div>
                     <div class="form-group">
                         <input type="text" class="form-input" name="password" id="password" placeholder="Password"/>
@@ -98,7 +41,7 @@ session_start();
                     </div>
                     <div class="form-group">
                         
-                        <input type="submit" name="submit" id="submit" class="form-submit submit" value="Log In"/>
+                        <input type="submit" name="login" id="submit" class="form-submit submit" value="Log In"/>
                         <a href="signupin.php" class="submit-link submit">Sign Up</a>
                     </div>
                     <div class="form-group">
--- Moderna-pro/colorlib-regform-9/colorlib-regform-9/signupin.php
+++ Moderna-pro/colorlib-regform-9/colorlib-regform-9/signupin.php
@@ -1,50 +1,3 @@
-<?php 
-session_start();
-
-	include("connection.php");
-	include("functions.php");
-
-
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-
-		if(!empty($email) && !empty($password))
-		{
-
-			//save to database
-			$user_id = random_num(20);
-			$query = "insert into customer (user_id,email,password) values ('$user_id','$email','$password')";
- 
-			mysqli_query($con, $query);
-
-			header("Location: CustomerLogin.php");
-			die;
-		}else
-		{
-			echo "Please enter some valid information!";
-		}
-	}
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,14 +19,14 @@ session_start();
 
         <div class="container">
             <div class="signup-content">
-                <form method="POST" id="signup-form" class="signup-form">
+                <form action=/MasterCLEO/Moderna-pro/colorlib-regform-9/colorlib-regform-9/CustomerDB.php method="POST" id="signup-form" class="signup-form">
                     <h2>Sign up </h2>
                     <p class="desc">Create a new account for <span>CLEO</span></p>
                     <div class="form-group">
                         <input type="text" class="form-input" name="name" id="name" placeholder="Username"/>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-input" name="email" id="text" placeholder="Email"/>
+                        <input type="text" class="form-input" name="email" id="email" placeholder="Email"/>
                     </div>
                     <div class="form-group">
                         <input type="password" class="form-input" name="password" id="password" placeholder="Password"/>
@@ -89,7 +42,9 @@ session_start();
                     </div>-->
                     <div class="form-group">
                         
-                        <input type="submit" name="submit" id="submit" class="form-submit submit" value="Sign up"/>
+                        <input type="submit" name="signup" id="signup" class="form-submit submit" value="Sign up"/>
+                        
+                        
                         <a href="CustomerLogin.php" class="submit-link submit">Log In</a>
                     </div>
                     <div class="form-group">

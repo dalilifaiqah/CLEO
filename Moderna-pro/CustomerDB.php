9975b5a99d3884aff0507d024a98f0f22e925df6
zeynanz <88524435+zeynanz@users.noreply.github.com>
2021-09-15 22:37:32 +0800

    Email verification

--- Moderna-pro/CustomerDB.php
+++ Moderna-pro/CustomerDB.php
@@ -26,19 +26,31 @@ if (isset($_POST['signup'])) {
         $name=$_POST['name'];
         $email=$_POST['email'];
         $password=$_POST['password'];
+        
+        $vkey = md5(time() . $name);
 
-        $sql = "INSERT INTO customer(name,email,password)
-                values('$name','$email','$password')";
+        $sql = "INSERT INTO customer(name,email,password,vkey)
+        values('$name','$email','$password','$vkey')";
+        }
+        
+            if ($con->query($sql) === TRUE) {
+                $to = $email;
+                $subject = "Verify Your Email Address";
+                $headers = "From: info.cleo@gmail.com\r\n";
+                $headers .= "MIME-Version : 1.0\r\n";
+                $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-                if ($con ->query($sql) ==true) {
+                $message = "<a href='http://localhost/MasterCLEO/Moderna-pro/Verify.php?vkey=$vkey'>Click Here To Verify Your Email Address</a>";
+
+                mail($to, $subject, $message, $headers);
+
+               
                      echo "Sign up successfully";
                      header("Location: /MasterCLEO/Moderna-pro/colorlib-regform-9/colorlib-regform-9/CustomerLogin.php");
                 }else{
                     echo "error";
                 }
-
-                
-        }
+       
     }
 
     function login(){
@@ -51,14 +63,24 @@ if (isset($_POST['signup'])) {
         $email = $sql->real_escape_string($_POST['email']);
         $password = $sql->real_escape_string($_POST['password']);
 
-        $result = $sql->query("SELECT * FROM customer WHERE email = '$email' AND password = '$password' ");
-        $count = mysqli_num_rows($result);
+        $resultSet = $sql->query("SELECT * FROM customer WHERE email = '$email' AND password = '$password' LIMIT 1 ");
 
-        if($count == 1){
-            //$row = $result->fetch_assoc();
-            header("Location: Guesthomepage.php");
-        }else{
-            echo '<br>Login failed';
+        if ($resultSet->num_rows != 0) {
+            //Process login
+            $row = $resultSet->fetch_assoc();
+            $verified = $row['verified'];
+            $email = $row['email'];
+    
+            if ($verified == 1) {
+                header("Location:/MasterCLEO/Moderna-pro/Guesthomepage.php");
+                           
+            } else {
+                echo  "Please verify your account and try again.";
+            }
+            
+        } else {
+            //Invalid login
+            echo "Invalid login Credentials";
         }
     }
 ?>
---
+++ Moderna-pro/Verify.php
@@ -0,0 +1,34 @@
+<?php
+if (isset($_GET['vkey'])){
+    //Process the vkey
+    $vkey = $_GET['vkey'];
+
+    $servername = "localhost";
+    $username = "admin1";
+    $password = "admin1";
+    $dbname = "cleo";
+    
+    $con = new mysqli($servername, $username, $password, $dbname);
+
+    $resultSet = $con->query("SELECT verified, vkey FROM customer WHERE verified = 0 AND vkey = '$vkey' LIMIT 1");
+
+    if ($resultSet->num_rows == 1) {
+        //validate the email address
+        $update = $con->query("UPDATE customer SET verified = 1 WHERE vkey = '$vkey' LIMIT 1");
+
+        if($update){
+            header("Location:/MasterCLEO/Moderna-pro/customer%20email%20verification/Emailsuccessful.html");
+        }else{
+            echo $mysqli->error;
+        }
+
+    }else{
+        echo "Error, This email address has been used or invalid. Please try other email address.";
+    }
+    
+}else{
+    die();
+}
+
+
+?>

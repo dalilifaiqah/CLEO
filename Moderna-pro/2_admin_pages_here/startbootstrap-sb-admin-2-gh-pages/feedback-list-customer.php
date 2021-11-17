a1513334f0a402a751816513feae1bb1e8136bc4
Aimil <88524549+MOHAMAD-AIMIL-MOHAMAD-ARIF@users.noreply.github.com>
2021-11-17 16:38:59 +0800

    Fix admin pages, customer and guest team pages

--- Moderna-pro/2_admin_pages_here/startbootstrap-sb-admin-2-gh-pages/feedback-list-customer.php
+++ Moderna-pro/2_admin_pages_here/startbootstrap-sb-admin-2-gh-pages/feedback-list-customer.php
@@ -277,6 +277,16 @@ $row=mysqli_fetch_array($query);
             </div>
             <!-- End of Main Content -->
 
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; CLEO 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
     </div>
     <!-- End of Page Wrapper -->
 

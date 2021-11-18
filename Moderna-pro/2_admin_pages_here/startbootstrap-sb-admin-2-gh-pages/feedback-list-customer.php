c1d1538207e60fee1c04ee15fce8df991efa865b
Aimil <88524549+MOHAMAD-AIMIL-MOHAMAD-ARIF@users.noreply.github.com>
2021-11-19 05:36:31 +0800

    admin feedback list profile

--- Moderna-pro/2_admin_pages_here/startbootstrap-sb-admin-2-gh-pages/feedback-list-customer.php
+++ Moderna-pro/2_admin_pages_here/startbootstrap-sb-admin-2-gh-pages/feedback-list-customer.php
@@ -209,7 +209,7 @@ $row=mysqli_fetch_array($query);
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="admin-profile.html">
+                                <a class="dropdown-item" href="admin-profile.php">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>

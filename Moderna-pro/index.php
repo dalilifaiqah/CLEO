0eb6c881452975461c0bdfa073b3d8fce7866fec
dalilifaiqah <88524466+dalilifaiqah@users.noreply.github.com>
2021-11-28 02:20:13 +0800

    update index.php

--- Moderna-pro/index.php
+++ Moderna-pro/index.php
@@ -48,35 +48,14 @@
 
       <div class="logo">
         <h1 style="font-family: Arial"class="text-light"><a href="index.php"><span>CLEO</span></a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!--<a href="index.html"><img src="assets/img/icon1.png" alt="" class="img-fluid"></a>-->
       </div>
 
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="active " href="index.php">Home</a></li>
           <li><a href="1about.html">About Us</a></li>
-          <!--<li><a href="services.html">Services</a></li>-->
           <li><a href="1products.html">Products</a></li>
           <li><a href="1team.html">Team</a></li>
-          <!--<li><a href="blog.html">Blog</a></li>-->
-          <!--<li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>-->
           <li><a href="1contact.html">Contact Us &nbsp;&nbsp;&nbsp;</a></li>
           <li><a href="colorlib-regform-9\colorlib-regform-9\CustomerLogin.php" class="sign-up-btn">Log in / Sign up</a></li>
         </ul>
@@ -95,8 +74,6 @@
         <div class="carousel-container">
           <img src="assets/img/Cleo (2).gif" class="img-fluid" alt="">
           <h2 class="animate__animated animate__fadeInDown">&nbsp &nbsp Welcome to <span>CLEO</span></h2>
-          <!--<p class="animate__animated animate__fadeInUp">CLEO is a place where Malaysian video gamers come together to explore and find their perfect in-game materials to bring up their in game experiences to the next level. </p>
-          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>-->
         </div>
       </div>
 
@@ -104,7 +81,8 @@
       <div class="carousel-item">
         <div class="carousel-container">
           <h2 class="animate__animated animate__fadeInDown">Game Accounts</h2>
-          <p class="animate__animated animate__fadeInUp">Feeling bad with your current game progress and have some extra cash in hand? No worries cause you can level up your game with top players' accounts that are sold!</p>
+          <p class="animate__animated animate__fadeInUp">Feeling bad with your current game progress and have some extra cash in hand?
+             No worries cause you can level up your game with top players' accounts that are sold!</p>
           <a href="1products.html" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>
         </div>
       </div>
@@ -140,7 +118,8 @@
             <div class="icon-box icon-box-pink">
               <div class="icon"><i class="fab fa-vuejs"></i></div>
               <h4 class="title"><a href="https://playvalorant.com/en-us/">VALORANT</a></h4>
-              <p class="description">Valorant is a team-based first-person hero shooter set in the near future. Players play as one of a set of agents, characters designed based on several countries and cultures around the world.</p>
+              <p class="description">Valorant is a team-based first-person hero shooter set in the near future.
+                 Players play as one of a set of agents, characters designed based on several countries and cultures around the world.</p>
             </div>
           </div>
 
@@ -148,7 +127,8 @@
             <div class="icon-box icon-box-cyan">
               <div class="icon"><i class="fas fa-meteor"></i></div>
               <h4 class="title"><a href="https://www.ea.com/en-gb/games/apex-legends">APEX LEGEND</a></h4>
-              <p class="description">Apex Legends is an online multiplayer battle royale game featuring squads of three players using pre-made characters with distinctive abilities, called "Legends", similar to those of hero shooters.</p>
+              <p class="description">Apex Legends is an online multiplayer battle royale game featuring squads of three players using 
+                pre-made characters with distinctive abilities, called "Legends", similar to those of hero shooters.</p>
             </div>
           </div>
 
@@ -156,14 +136,17 @@
             <div class="icon-box icon-box-green">
               <div class="icon"><i class="fas fa-fighter-jet"></i></i></div>
               <h4 class="title"><a href="https://www.pubgmobile.com/en-US/home.shtml">PUBG MOBILE</a></h4>
-              <p class="description">PlayerUnknown's Battleground or PUBG is a battle royale game. In a battle royale game, a player is left with up to 100 other players in a large map.</p>
+              <p class="description">PlayerUnknown's Battleground or PUBG is a battle royale game. In a battle royale game, a player is left
+                 with up to 100 other players in a large map.</p>
             </div>
           </div>
 
           <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
             <div class="icon-box icon-box-pink">
               <div class="icon"><i class="fas fa-gamepad"></i></div>
-              <h4 class="title"><a href="https://genshin.mihoyo.com/pc-launcher/?utm_source=SEA_google_NZMY_SEM_brand_1022&mhy_trace_channel=ga_channel&new=1&gclid=Cj0KCQjwkIGKBhCxARIsAINMioKl-4sQkEmHOLFYt374rGdGxG-802liQPoAmNg9OZ7W7nXsTSjwFQQaApcTEALw_wcB#/">GENSHIN IMPACT</a></h4>
+              <h4 class="title">
+              <a href="https://genshin.mihoyo.com/pc-launcher/?utm_source=SEA_google_NZMY_SEM_brand_1022&mhy_trace_channel=ga_channel&new=1&gclid=Cj0KCQjwkIGKBhCxARIsAINMioKl-4sQkEmHOLFYt374rGdGxG-802liQPoAmNg9OZ7W7nXsTSjwFQQaApcTEALw_wcB#/">
+                GENSHIN IMPACT</a></h4>
               <p class="description">Genshin Impact is an open-world action role-playing game that allows the player to control one of four interchangeable characters in a party.</p>
             </div>
           </div>
@@ -214,107 +197,10 @@
       </div>
     </section><!-- End Why Us Section -->
 
-    <!-- ======= Features Section ======= -->
-    <!--<section class="features whiteText">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Game Info</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-
-        <div class="row" data-aos="fade-up">
-          <div class="col-md-5">
-            <img src="assets/img/features-1.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-7 pt-4">
-            <h3>VALORANT</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <ul>
-              <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="row" data-aos="fade-up">
-          <div class="col-md-5 order-1 order-md-2">
-            <img src="assets/img/features-2.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-7 pt-5 order-2 order-md-1">
-            <h3>Corporis temporibus maiores provident</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </div>
-        </div>
-
-        <div class="row" data-aos="fade-up">
-          <div class="col-md-5">
-            <img src="assets/img/features-3.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-7 pt-5">
-            <h3>Sunt consequatur ad ut est nulla consectetur reiciendis animi voluptas</h3>
-            <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>
-            <ul>
-              <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="bi bi-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="row" data-aos="fade-up">
-          <div class="col-md-5 order-1 order-md-2">
-            <img src="assets/img/features-4.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-7 pt-5 order-2 order-md-1">
-            <h3>Quas et necessitatibus eaque impedit ipsum animi consequatur incidunt in</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </section> --><!-- End Features Section -->
-
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
   <footer id="footer" class="section-bg2" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
-
-    <!--<div class="footer-newsletter">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-          </div>
-          <div class="col-lg-6">
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>-->
-
     <div class="footer-top">
       <div class="container">
         <div class="row">
